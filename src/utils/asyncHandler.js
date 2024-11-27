a65fd527b4bdb6b791bd69332919d43fd97a1d85
Irfan140 <irfanmehmud140@gmail.com>
2024-11-27 18:59:37 +0530

    error handle

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,40 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+/*
+
+1. When we want to perform some task before reciving the request from the frontend we use backend
+2. In utils we make  function for general purpose use...in our utils folder
+3. basic of hogher order function
+const asyncHandler = () => {}
+const asyncHandler = (func) => () => {}
+const asyncHandler = (func) => async () => {}
+
+
+
+*/
+
+
+
+//? Another way of writting asyncHandler in try catch way
+
+/*
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+
+*/
