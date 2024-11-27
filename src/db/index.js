3992e5ce0c722814abae80679efdcf421b5acc65
Irfan140 <irfanmehmud140@gmail.com>
2024-11-27 16:52:39 +0530

    modified

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 
 const connectDB = async () => {
+    
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
@@ -13,9 +14,8 @@ const connectDB = async () => {
 }
 
 export default connectDB
-
 /* 
-1. process is from NOde js (Read more about it and type s of exit)
+1. process is from Node js (Read more about it and types of exit)
 2. Console log connectionInstance
 3. 
 */
