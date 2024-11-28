94901f6b432af9ac45ea464863b822d9caf81dd4
Irfan140 <irfanmehmud140@gmail.com>
2024-11-28 10:28:10 +0530

    video model created

---
+++ src/models/video.model.js
@@ -0,0 +1,54 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, // cloudinary url
+            required: true // for default message use in array format like [true, "required field"]
+        },
+        thumbnail: {
+            type: String, // cloudinary url
+            required: true
+        },
+        title: {
+            type: String, 
+            required: true
+        },
+        description: {
+            type: String, 
+            required: true
+        },
+        duration: {
+            type: Number, 
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
+
+
+/*
+1. mongoose-aggregate-paginate-v2 allows us to write aggregration quesries to solve complexity
+
+
+*/
