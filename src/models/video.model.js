d6c9e6f39a3576f2e8e6502a63ff4574201d09eb
Irfan140 <irfanmehmud140@gmail.com>
2024-11-29 10:10:43 +0530

    comments added

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -4,11 +4,11 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema(
     {
         videoFile: {
-            type: String, // cloudinary url
-            required: true // for default message use in array format like [true, "required field"]
+            type: String, // from cloudinary url
+            required: true // for any default message use an array format like [true, "Any message"]
         },
         thumbnail: {
-            type: String, // cloudinary url
+            type: String, // from cloudinary url
             required: true
         },
         title: {
@@ -48,7 +48,9 @@ export const Video = mongoose.model("Video", videoSchema)
 
 
 /*
-1. mongoose-aggregate-paginate-v2 allows us to write aggregration quesries to solve complexity
+1. Aggregation queries are like advanced searches in the database. They let you group, filter, or calculate data in complicated ways, such as finding the total views for all videos by a user or sorting videos by popularity.
+
+2. The mongoose-aggregate-paginate-v2 plugin makes it easy to break these large search results into smaller, manageable chunks (pages). This is useful when you want to show results one page at a time, like displaying 10 videos per page on a website.
 
 
 */
