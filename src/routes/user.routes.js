52830d918a6fcbbf9d9910161c8a906b28cd9f60
Irfan140 <irfanmehmud140@gmail.com>
2024-11-30 09:22:50 +0530

    modified

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,26 @@
 import { Router } from "express" // Imports the Router class from the express library
 import { registerUser } from "../controllers/user.controller.js"
+import {upload} from "../middlewares/multer.middleware.js"
 
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+// means when we get a request at register route then registerUser get executed
+router.route("/register").post(
+    // middleware 
+    // when we make the frontend then its field name must be same as below mentioned names
+    upload.fields([
+        {
+            name: "avatar", 
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 
 
 export default router
@@ -25,6 +41,6 @@ It contains the business logic for handling the registration process, like valid
 -> router.route("/register"): Specifies the /register endpoint.
 .post(registerUser): Attaches a POST method to this route, so it listens only for HTTP POST requests. When a POST request is made to /register, the registerUser function is called.
 
-
+-> middleware are like jate huwa mujhse milkr jana
 
 */
